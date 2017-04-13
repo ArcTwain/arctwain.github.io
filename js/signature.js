61a4db0272c18985f518ca0bd3b0dc23dc619a8f
Tina Anastopoulos <arctwain@gmail.com>
2017-04-13 16:43:11 -0400

    Create signature.js

---
+++ js/signature.js
@@ -0,0 +1,9 @@
+var mySignature = document.getElementById('signature');
+
+var waypoint = new Waypoint({ 
+    element: mySignature,
+    handler: function (direction) {
+        mySignature.classList.add('animate')
+    },
+    offset: 'bottom-in-view'
+})
