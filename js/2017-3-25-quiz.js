1d028b73445c22ae301df2a9220e5cd51f5fb7a3
Tina Anastopoulos <arctwain@gmail.com>
2017-04-14 20:34:44 -0400

    Create 2017-3-25-quiz.js

---
+++ js/2017-3-25-quiz.js
@@ -0,0 +1,9 @@
+// Values for popquiz.js
+// Object contains holds multiple-choice questions
+// Last digit in the array gives the right answer position
+
+var allQuestions = {
+	'How many hexadecimal color codes are there?' : ['12,048,576', '16,016,016', '16,777,216', 2],
+	'What is the number after F in hexadecimal?' : ['FF', '10' , '11', 1],
+	'How many colors can humans see? ' : ['7,000,000', '12,000,000', '17,000,000', 0]
+ };
